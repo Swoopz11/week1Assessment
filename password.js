542a9ad5b83d88fc351cc84b9dc26d28c7bd6536
Hunter Birch <hunter.macka.birch@gmail.com>
2022-05-27 12:19:40 -0600

    my best attempt at art

--- password.js
+++ password.js
@@ -11,12 +11,17 @@ reader.question("Enter password for validation: ", function(input){
 	const password = input;
 	
 	if (password.length >= 10) {
-        console.log('Password validated!')
+        console.log('Password validated!') 
+        console.log(' | |')
+        console.log('(___)')
     } else if (password.length === 0) {
         console.log('No password to validate')
+        console.log('0 0')
+        console.log('-U-')
     } else if (password.length < 10) {
         console.log('Password is not long enough')
-    
+        console.log(' U U')
+        console.log('(---)')
     }
 
     reader.close()
