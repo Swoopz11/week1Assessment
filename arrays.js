b7c9e7957550f5dee2c8c63bdb03db5fdd7d16ba
Hunter Birch <hunter.macka.birch@gmail.com>
2022-05-27 10:37:48 -0600

    Arrays, finished

--- arrays.js
+++ arrays.js
@@ -3,12 +3,15 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+let colorCopy = faveColors.slice(0, faveColors.length)
+
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push('blue')
+
+console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,8 +21,9 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
+let middleNums = numbers.splice(1,3)
 
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,4 +38,16 @@ const numbers = [1, 2, 3, 4, 5]
   Return the answers array inside of the function.
 */
 
-// CODE HERE
+function bigOrSmall(arr) {
+  let answers = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 100) {
+      answers.push('big')
+    } else if (arr[i] <= 100) {
+      answers.push('small')
+    }
+  }
+  return answers
+}
+
+console.log(bigOrSmall(numbers))
